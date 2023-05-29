0dd5783bff71509cf39d1a32262abdaf719caac4
Oleg <oleg_goncharov@inbox.ru>
2023-05-29 21:45:24 +0300

    menu-edit

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
 
-Array.from(document.querySelectorAll(".menu")).forEach((item) => {
+// Array.from(document.querySelectorAll(".menu")).forEach((item) => {
+//   item.onclick = () => {
+//     item.parentElement.parentElement.classList.remove("change");
+//   };
+// });
+
+Array.from(document.querySelectorAll(".menu-link")).forEach((item) => {
   item.onclick = () => {
-    item.parentElement.parentElement.classList.remove("change");
+    document.querySelector(".container").classList.remove("change");
   };
 });
-
 // Siebar End
+
+// Accordion
+let acc = document.querySelectorAll(".accordion"),
+  i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    let desc = this.nextElementSibling;
+    this.classList.toggle("active");
+    desc.style.maxHeight
+      ? (desc.style.maxHeight = null)
+      : (desc.style.maxHeight = desc.scrollHeight + "px");
+  });
+}
+// Accordion End
